e0a2338bde9148bc1bc4235b41abbe980313a7a9
Aryan20apr <aryansingh20nov@gmail.com>
2024-09-21 23:23:35 +0530

    Form Fieled entity and enums for form field type

--- src/main/java/com/aryansingh/instaforms/models/entities/form/content/FormField.java
+++ src/main/java/com/aryansingh/instaforms/models/entities/form/content/FormField.java
@@ -1,19 +1,23 @@
 package com.aryansingh.instaforms.models.entities.form.content;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 
+import java.sql.Date;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
+@ToString
 public class FormField {
 
     @Id
@@ -25,5 +29,43 @@ public class FormField {
 
     @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL,orphanRemoval = true)
     private List<SectionResource> resources = new ArrayList<>();
+
+    @Enumerated(EnumType.STRING)
+    FormFieldType type;
+
+    Boolean required;
+
+    String fieldToken;
+
+    @ManyToOne
+    @JoinColumn(name = "section_id", referencedColumnName = "section_id")
+    Section section;
+
+    @Column(name = "seq_no")
+    Integer sequenceNumber;
+
+    @CreatedDate
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private Date createdAt;
+
+    @LastModifiedDate
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private Date updatedAt;
+
+    @PrePersist
+    public void generateToken() {
+        if (this.fieldToken == null) {
+            this.fieldToken = UUID.randomUUID().toString();
+        }
+    }
+
+    // TODO: Style configuration
+
+
+
+
+
 
 }
---
+++ src/main/java/com/aryansingh/instaforms/models/entities/form/content/FormFieldType.java
@@ -0,0 +1,12 @@
+package com.aryansingh.instaforms.models.entities.form.content;
+
+public enum FormFieldType {
+
+    TEXT,
+    NUMBER,
+    FILE,
+    DATE,
+    TIME,
+    MULTI_SELECT,
+    SINGLE_SELECT
+}
